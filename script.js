42db27a80078369004accde5bb87efe98bacf642
Kasula Saitejachary <98974519+kasulasaitejachary@users.noreply.github.com>
2024-07-02 00:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const outputDiv = document.getElementById("output");
+const inputValue = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", delayOutput);
+
+async function delayOutput(e) {
+	e.preventDefault;
+  return await setTimeout(()=>{
+		outputDiv.innerHTML = inputValue.target.value;
+	}, delay.target.value);
+}
